1e9cc69f08d4aae1ca42a069d2cdcc3378dda8df
M Rifqi <muhamadrifqi.id@gmail.com>
2025-07-02 14:52:37 +0700

    logout

--- src/components/PrivateNavbar.jsx
+++ src/components/PrivateNavbar.jsx
@@ -1,5 +1,21 @@
-import { Navigate, NavLink } from "react-router-dom";
+import { Navigate, NavLink, useNavigate } from "react-router-dom";
+import { Slide, toast } from "react-toastify";
 const PrivateNavbar = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    window.localStorage.removeItem("blogData");
+    toast.success("Logout successfully", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      theme: "light",
+      transition: Slide,
+    });
+    navigate("/login");
+  };
   return (
     <nav>
       <NavLink to={"/"}>Home</NavLink>
@@ -7,7 +23,58 @@ const PrivateNavbar = () => {
       <NavLink to={"/posts"}>Posts</NavLink>
       <NavLink to={"/profile"}>Profile</NavLink>
       <NavLink to={"/setting"}>Setting</NavLink>
-      <NavLink to={"/logout"}>Logout</NavLink>
+      <NavLink
+        to={"/login"}
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Logout
+      </NavLink>
+      {/* <button type="button" class="btn btn-primary">
+        Launch demo modal
+      </button> */}
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Are you sure?
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            {/* <div class="modal-body">...</div> */}
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-bs-dismiss="modal"
+              >
+                No
+              </button>
+              <button
+                data-bs-dismiss="modal"
+                type="button"
+                onClick={logout}
+                class="btn btn-success"
+              >
+                Yes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };

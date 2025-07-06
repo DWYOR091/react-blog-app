8cff1aacb7c2a85dc76e852914d497dcaa1f4775
M Rifqi <muhamadrifqi.id@gmail.com>
2025-07-06 16:38:49 +0700

    Home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,106 @@
+import { useEffect, useState } from "react";
+import axios from "../utils/axiosInstance";
+import { useNavigate } from "react-router-dom";
+import langit from "../assets/images/langit.png";
+
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [totalPage, setTotalPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTitle, setSearchTitle] = useState("");
+  const size = 6;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getDataPost = async () => {
+      try {
+        setloading(true);
+        const response = await axios.get(
+          `post?title=${searchTitle}&size=${size}&page=${currentPage}`
+        );
+        setPosts(response.data.data.posts);
+        setTotalPage(response.data.pages);
+        setloading(false);
+      } catch (error) {
+        setloading(false);
+        console.log(error);
+      }
+    };
+    getDataPost();
+  }, [currentPage, searchTitle]);
+
+  const handleSearch = (e) => {
+    setSearchTitle(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const handlePrev = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
   return (
     <div className="container">
-      <p>This is home page</p>
+      <h2 className="text-center my-5">Post List</h2>
+      search <input type="text" className="mb-3" onChange={handleSearch} />
+      <br />
+      {loading ? (
+        <h1 className="mt-5 text-center">Loading ...</h1>
+      ) : totalPage > 0 ? (
+        <>
+          <div className="mt-3 mb-3 d-flex flex-wrap justify-content-around">
+            {posts.map((p) => {
+              return (
+                <div
+                  className="card my-3"
+                  style={{ width: "18rem" }}
+                  key={p._id}
+                  onClick={() => navigate(`/posts/detailPost/${p._id}`)}
+                >
+                  <img
+                    src={
+                      p.file
+                        ? `http://localhost:5000/image/${p.file.name}`
+                        : langit
+                    }
+                    className="card-img-top"
+                    alt="..."
+                    style={{ objectFit: "cover", height: "180px" }}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.title}</h5>
+                    <p className="card-text">{p.desc}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="">
+            <button
+              className="btn btn-secondary"
+              onClick={handlePrev}
+              disabled={currentPage === 1}
+            >
+              Prev
+            </button>
+            <span className="mx-3">
+              Page {currentPage} of {totalPage}
+            </span>
+            <button
+              className="btn btn-secondary"
+              onClick={handleNext}
+              disabled={currentPage === totalPage}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      ) : (
+        <p className="text-center fw-bold h4 mt-5">Data not found</p>
+      )}
     </div>
   );
 };
